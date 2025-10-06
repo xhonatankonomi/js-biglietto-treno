8ad97f0bf6342cb3bde99b86e0e2c78f5941e0ba
xhonatankonomi <xhoni8962@gmail.com>
2025-10-06 14:37:00 +0200

    feat: Add km input and console log
    This commit introduces a prompt for user input regarding the number of kilometers and logs the input to the console. This allows users to specify the distance for the fare calculation.

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,11 +1,16 @@
 // inizializzo le costanti
-    // dichiaro una variabile float per l'output
+// dichiaro una variabile float per l'output
+
+// Le costanti servono a stabilire i prezzi fissi delle tariffe. Servono a impedire che possano variare in seguito
 
 const eur_km = 0.21;
 const children = eur_km - (eur_km * 0.2)
 const senior = eur_km - (eur_km * 0.4)
 
 // Richiedo l'input km all'utente
+
+let km = prompt("Inserisci i km che percorrerai: ")
+console.log(km)
 
 // richiedi l'età
 
