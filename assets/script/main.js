214d23f3f66e696227337b3118ec8a2140f4f7e5
xhonatankonomi <xhoni8962@gmail.com>
2025-10-06 14:56:55 +0200

    feat: Implement age-based ticket price calculation
    This commit introduces age-based pricing logic.
    It now considers user's age (child, senior, adult)
    and calculates the price accordingly.

--- assets/script/main.js
+++ assets/script/main.js
@@ -26,18 +26,31 @@ let età;
 età = prompt("Inserisci la tua età:")
 console.log(età)
 
-// Confronto l'età
-
 alert("Vedo che hai " + età + " anni! Li porti bene. Adesso farò i calcoli e ti dirò quanto spenderai!")
 
+// condizioni di confronto delle età
+
 if (età <= 18) {
-   output_price = km * children;
+
+    output_price = km * children;
     output_price = output_price.toFixed(2); 
     console.log(output_price);
+    alert("Dato che hai " + età + " anni, pagherai " + output_price + "€")
+
+}  
+    else if (età >= 65) {
+
+        output_price = km * senior;
+        output_price = output_price.toFixed(2); 
+        console.log(output_price);
+        alert("Dato che hai " + età + " anni, pagherai " + output_price + "€")
+
+}  
+    else {
+
+        output_price = km * eur_km;
+        output_price = output_price.toFixed(2); 
+        console.log(output_price);
+        alert("Dato che hai " + età + " anni, pagherai " + output_price + "€")
+
 }
-
-// condizione età minore di 18
-
-// condizione età maggiore di 65
-
-//output
