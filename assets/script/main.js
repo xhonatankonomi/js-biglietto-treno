095b00c554cef315a35e7615f53a004b4739ba82
xhonatankonomi <xhoni8962@gmail.com>
2025-10-06 14:28:39 +0200

    Initialize constants and input variables
    This commit initializes constants and input variables to prepare for calculations. It sets up the structure for requesting kilometer input and age input from the user, and prepares for comparison and output logic.

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,15 @@
-// 
+// inizializzo le costanti
+    // dichiaro una variabile float per l'output
+    
+
+// Richiedo l'input km all'utente
+
+// richiedi l'età
+
+// Confronto l'età
+
+// condizione età minore di 18
+
+// condizione età maggiore di 65
+
+//output
